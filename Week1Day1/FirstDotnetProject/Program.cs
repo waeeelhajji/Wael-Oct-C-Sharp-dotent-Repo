639f38235c5ff2d9be7f0bf78855ec097879f7c7
Wael Hajji <waelhajji.contact@gmail.com>
2023-10-25 08:21:47 +0100

    Week1Day1

---
+++ Week1Day1/FirstDotnetProject/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello and Welcome to the C# .Net Stack");
+
+//Javascript 
+// var varName = "hello"
+
+// Python 
+// name = "hello"
+
+
+
+// C#
+string FirstVariable = "Welcome to C#";
+
+Console.WriteLine(FirstVariable);
+
+// JS Js function
+// function functionName(){}
+
+//C# function 
+// you need to specify what will be returning by the function
+
+static void functionName()
+{
+    // void is the term we use when there is no Return Statement
+}
+
+static string Hello()
+{
+    return "Hello";
+}
+
